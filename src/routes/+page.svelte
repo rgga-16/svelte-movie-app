ea3120874269a882ed9d81a52b8239772c558c6c
Rgee Gallega <rgga16@gmail.com>
2023-01-13 17:35:53 +0900

    Added other pages for routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation. Idk how to add other things.</p>
